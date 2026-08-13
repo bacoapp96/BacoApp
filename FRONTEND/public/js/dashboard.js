12b108e95720c00e5b775a4f05cf8c9234bc8076
Jaider Anaya <jaideralmanza@gmail.com>
2026-08-13 16:53:26 -0500

    Corregir peticiones del dashboard

--- FRONTEND/public/js/dashboard.js
+++ FRONTEND/public/js/dashboard.js
@@ -7,9 +7,9 @@ async function loadDashboard() {
             resClientes,
             resVentas
         ] = await Promise.all([
-            fetch("https://bacoapp-production.up.railway.app/api/productos"),
-            fetch("https://bacoapp-production.up.railway.app/api/clientes"),
-            fetch("https://bacoapp-production.up.railway.app/api/ventas")
+            fetch("/api/productos"),
+            fetch("/api/clientes"),
+            fetch("/api/ventas")
         ]);
 
         const productos = await resProductos.json();
@@ -36,7 +36,7 @@ async function cargarUltimosPedidos() {
 
     try {
         
-        const response = await fetch("https://bacoapp-production.up.railway.app/api/ventas");
+        const response = await fetch("/api/ventas");
         const ventas = await response.json();
 
         const lista = document.getElementById("ordersList");
@@ -63,7 +63,7 @@ async function cargarStockBajo() {
 
     try {
 
-        const response = await fetch("https://bacoapp-production.up.railway.app/api/productos/stock-bajo");
+        const response = await fetch("/api/productos/stock-bajo");
         const productos = await response.json();
 
         const lista = document.getElementById("stockList");
