7b3c31a66bff0e4d8b9a16841e7b7000c5982504
Jaider Anaya <jaideralmanza@gmail.com>
2026-08-15 19:56:21 -0500

    Console para SMTP

--- BACKEND/app/config/mail.js
+++ BACKEND/app/config/mail.js
@@ -3,6 +3,13 @@ dotenv.config();
 
 import nodemailer from "nodemailer";
 
+console.log("========== SMTP CONFIG ==========");
+console.log("SMTP_HOST:", process.env.SMTP_HOST);
+console.log("SMTP_PORT:", process.env.SMTP_PORT);
+console.log("SMTP_USER:", process.env.SMTP_USER);
+console.log("SMTP_PASS EXISTE:", !!process.env.SMTP_PASS);
+console.log("=================================");
+
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
